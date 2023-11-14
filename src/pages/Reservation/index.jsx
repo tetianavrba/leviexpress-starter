de308d8a5edf71eb17aa79c073f1be733ba48ec9
Tetiana Vrba <tetiana.vrba@gmail.com>
2023-11-14 17:53:44 +0100

    ReservationPage Api

--- src/pages/Reservation/index.jsx
+++ src/pages/Reservation/index.jsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import './style.css';
+
 export const ReservationPage = () => {
+  const { id } = useParams();
+  const [reservation, setReservation] = useState(null);
+  useEffect(() => {
+    const fetchReservation = async () => {
+      const resp = await fetch(
+        `https://apps.kodim.cz/daweb/leviexpress/api/reservation?id=${id}`,
+      );
+      if (!resp.ok) {
+        alert('Aj, karamba! Něco se pokazilo. Server se mnou nemluví 😢');
+        return;
+      }
+      const data = await resp.json();
+      setReservation(data.results);
+    };
+
+    fetchReservation();
+  }, []);
   return (
-    <>
-      <h1>Rezervace</h1>
-    </>
+    <div className="reservation container">
+      <h2>Vaše e-jízdenka č. {id}</h2>
+      {reservation && (
+        <div className="reservation__body">
+          <div className="reservation__headings">
+            <p>Datum cesty:</p>
+            <p>Odjezd:</p>
+            <p>Příjezd:</p>
+            <p>Sedadlo:</p>
+          </div>
+          <div className="reservation__info">
+            <p>{reservation.date}</p>
+            <p>
+              {reservation.fromCity.name}, {reservation.fromCity.time}
+            </p>
+            <p>
+              {reservation.toCity.name}, {reservation.toCity.time}
+            </p>
+            <p>{reservation.seatNumber}</p>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
